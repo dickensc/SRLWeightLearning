d4c59600fbe570fc0afe7ea8821e27d0679cc681
Charles Dickens <charlieadickens@gmail.com>
2020-03-05 15:32:09 -0800

    pass seed as arg to search methods

--- run_all_CRGS.sh
+++ run_all_CRGS.sh
@@ -1,1 +1,3 @@
-for i in 0 1 2 3 4 5 6 7 ; do echo "---------------Runnning : $i------------------"; ./run_i_CRGS.sh ${i}; done
+seed=4
+
+for i in 0 1 2 3 4 5 6 7 ; do echo "---------------Runnning : $i------------------"; ./run_i_CRGS.sh ${i} ${seed}; done
--- run_all_RGS.sh
+++ run_all_RGS.sh
@@ -1,1 +1,3 @@
-for i in 0 1 2 3 4 5 6 7 ; do echo "---------------Runnning : $i------------------"; ./run_i_RGS.sh ${i}; done
+seed=4
+
+for i in 0 1 2 3 4 5 6 7 ; do echo "---------------Runnning : $i------------------"; ./run_i_RGS.sh ${i} ${seed}; done
