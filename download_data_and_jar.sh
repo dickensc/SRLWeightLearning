c6daaae596cc90abf5a34d4425f55c9536b49273
Charles Dickens <charlieadickens@gmail.com>
2020-03-05 15:58:36 -0800

    formatting and remove unnecessary jar fetch

--- download_data_and_jar.sh
+++ download_data_and_jar.sh
@@ -2,8 +2,6 @@ readonly PSL_VERSION='2.3.0-SNAPSHOT'
 readonly JAR_PATH="./psl-cli-${PSL_VERSION}.jar"
 snapshotJARPath="$HOME/.m2/repository/org/linqs/psl-cli/${PSL_VERSION}/psl-cli-${PSL_VERSION}.jar"
 cp "${snapshotJARPath}" "${JAR_PATH}"
-tar -zxf y4rceuzg
-rm y4rceuzg
 wget -q https://tinyurl.com/y6hqz57a
 mv y6hqz57a psl-cli-2.2.0-SNAPSHOT.jar
 wget -q https://tinyurl.com/y5s8vacr
--- run_i_LME.sh
+++ run_i_LME.sh
@@ -1,12 +1,25 @@
-./LME.sh Citeseer.psl Citeseer-learn-${1}.data ${2} ${3}; cp Citeseer-learned.psl Citeseer-learned-LME-${1}.psl; ./inference.sh Citeseer-learned-LME-${1}.psl Citeseer-eval-${1}.data Categorical LME; ./inference.sh Citeseer-learned-LME-${1}.psl Citeseer-eval-${1}.data Discrete LME;
+./LME.sh Citeseer.psl Citeseer-learn-${1}.data ${2} ${3};
+cp Citeseer-learned.psl Citeseer-learned-LME-${1}.psl;
+./inference.sh Citeseer-learned-LME-${1}.psl Citeseer-eval-${1}.data Categorical LME;
+./inference.sh Citeseer-learned-LME-${1}.psl Citeseer-eval-${1}.data Discrete LME;
 
-./LME.sh Cora.psl Cora-learn-${1}.data ${2} ${3}; cp Cora-learned.psl Cora-learned-LME-${1}.psl; ./inference.sh Cora-learned-LME-${1}.psl Cora-eval-${1}.data Categorical LME; ./inference.sh Cora-learned-LME-${1}.psl Cora-eval-${1}.data Discrete LME;
+./LME.sh Cora.psl Cora-learn-${1}.data ${2} ${3}; cp Cora-learned.psl Cora-learned-LME-${1}.psl;
+./inference.sh Cora-learned-LME-${1}.psl Cora-eval-${1}.data Categorical LME;
+./inference.sh Cora-learned-LME-${1}.psl Cora-eval-${1}.data Discrete LME;
 
-./LME.sh Epinions.psl Epinions-learn-${1}.data ${2} ${3}; cp Epinions-learned.psl Epinions-learned-LME-${1}.psl; ./inference.sh Epinions-learned-LME-${1}.psl Epinions-eval-${1}.data Ranking LME; ./inference.sh Epinions-learned-LME-${1}.psl Epinions-eval-${1}.data Discrete LME;
+./LME.sh Epinions.psl Epinions-learn-${1}.data ${2} ${3};
+cp Epinions-learned.psl Epinions-learned-LME-${1}.psl;
+./inference.sh Epinions-learned-LME-${1}.psl Epinions-eval-${1}.data Ranking LME;
+./inference.sh Epinions-learned-LME-${1}.psl Epinions-eval-${1}.data Discrete LME;
 
 if ${1}<=4
 then
-  ./LME.sh LastFM.psl LastFM-learn-${1}.data ${2} ${3}; cp LastFM-learned.psl LastFM-learned-LME-${1}.psl; ./inference.sh LastFM-learned-LME-${1}.psl LastFM-eval-${1}.data Ranking LME; ./inference.sh LastFM-learned-LME-${1}.psl LastFM-eval-${1}.data Continuous LME;
+  ./LME.sh LastFM.psl LastFM-learn-${1}.data ${2} ${3}; cp LastFM-learned.psl LastFM-learned-LME-${1}.psl;
+  ./inference.sh LastFM-learned-LME-${1}.psl LastFM-eval-${1}.data Ranking LME;
+  ./inference.sh LastFM-learned-LME-${1}.psl LastFM-eval-${1}.data Continuous LME;
 fi
 
-./LME.sh Jester.psl Jester-learn-${1}.data ${2} ${3}; cp Jester-learned.psl Jester-learned-LME-${1}.psl; ./inference.sh Jester-learned-LME-${1}.psl Jester-eval-${1}.data Ranking LME; ./inference.sh Jester-learned-LME-${1}.psl Jester-eval-${1}.data Continuous LME;
+./LME.sh Jester.psl Jester-learn-${1}.data ${2} ${3};
+cp Jester-learned.psl Jester-learned-LME-${1}.psl;
+./inference.sh Jester-learned-LME-${1}.psl Jester-eval-${1}.data Ranking LME;
+./inference.sh Jester-learned-LME-${1}.psl Jester-eval-${1}.data Continuous LME;
