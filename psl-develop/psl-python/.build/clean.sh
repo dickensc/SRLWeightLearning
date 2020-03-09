cd00b5cf25c66b53826a8a253d52f599943867ac
Charles Dickens <charlieadickens@gmail.com>
2020-03-09 16:15:08 -0700

    remove unecessary PSL dir

--- psl-develop/psl-python/.build/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Clean the package of the artifacts from any build step.
-
-rm -Rf build dist pslpython.egg-info
-find . -name __pycache__ | xargs rm -Rf
--- psl-develop/psl-python/.build/package.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create a Python binary wheel.
-# This will clear out any previous builds.
-
-rm -Rf dist
-python3 setup.py bdist_wheel
