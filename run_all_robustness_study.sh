754d868c2b5399067c03f74ced9f2bb746a10a86
cadicken@ucsc.edu <cadicken@ucsc.edu>
2020-03-09 17:23:05 -0700

    robustness study infrastructure

--- run_all_robustness_study.sh
+++ run_all_robustness_study.sh
@@ -4,12 +4,67 @@
 echo "Running Robustness study on all methods. This might take a few days to finish...."
 
 n=100
-fold=1
+fold=0
 weightlearning_randomweights="true"
+datasets='Citeseer Cora Epinions Jester LastFM'
+metrics='Categorical Discrete Continuous Ranking'
 
-# clear and make new weight files for each dataset and weightlearning method and metric
+for method in $methods; do
+    for j in Categorical Discrete; do
+        # Citeseer 
+        # clear and make new weight files for each dataset and weightlearning method and metric
+        rm Citeseer/Citeseer/robustness_weights_${method}_${j}.csv
+        touch Citeseer/Citeseer/robustness_weights_${method}_${j}.csv
 
-# clear and make new evaluation files for each dataset and weightlearning method and metric
+        # clear and make new evaluation files for each dataset and weightlearning method and metric
+        rm Citeseer/Citeseer/robustness_evaluations_${method}_${j}.csv
+        touch Citeseer/Citeseer/robustness_evaluations_${method}_${j}.csv
+    done
+
+    for j in Categorical Discrete; do
+        # Cora 
+        # clear and make new weight files for each dataset and weightlearning method and metric
+        rm Cora/Cora/robustness_weights_${method}_${j}.csv
+        touch Cora/Cora/robustness_weights_${method}_${j}.csv
+
+        # clear and make new evaluation files for each dataset and weightlearning method and metric
+        rm Cora/Cora/robustness_evaluations_${method}_${j}.csv
+        touch Cora/Cora/robustness_evaluations_${method}_${j}.csv
+    done
+
+    for j in Ranking Discrete; do
+        # Epinions 
+        # clear and make new weight files for each dataset and weightlearning method and metric
+        rm Epinions/Epinions/robustness_weights_${method}_${j}.csv
+        touch Epinions/Epinions/robustness_weights_${method}_${j}.csv
+
+        # clear and make new evaluation files for each dataset and weightlearning method and metric
+        rm Epinions/Epinions/robustness_evaluations_${method}_${j}.csv
+        touch Epinions/Epinions/robustness_evaluations_${method}_${j}.csv
+    done
+
+    for j in Continuous Ranking; do
+        # LastFM 
+        # clear and make new weight files for each dataset and weightlearning method and metric
+        rm LastFM/LastFM/robustness_weights_${method}_${j}.csv
+        touch LastFM/LastFM/robustness_weights_${method}_${j}.csv
+
+        # clear and make new evaluation files for each dataset and weightlearning method and metric
+        rm LastFM/LastFM/robustness_evaluations_${method}_${j}.csv
+        touch LastFM/LastFM/robustness_evaluations_${method}_${j}.csv
+    done
+
+    for j in Continuous Ranking; do
+        # Jester 
+        # clear and make new weight files for each dataset and weightlearning method and metric
+        rm Jester/Jester/robustness_weights_${method}_${j}.csv
+        touch Jester/Jester/robustness_weights_${method}_${j}.csv
+
+        # clear and make new evaluation files for each dataset and weightlearning method and metric
+        rm Jester/Jester/robustness_evaluations_${method}_${j}.csv
+        touch Jester/Jester/robustness_evaluations_${method}_${j}.csv
+    done
+done
 
 # iterate will seed the random initial weights or search or both, depending on the method
 for (( seed=1; seed<=n; seed++ )) do
