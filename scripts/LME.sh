7386cf1b002d8eca6fc899e420c8944e7ad9519a
Charles Dickens <charlieadickens@gmail.com>
2020-04-04 14:26:34 -0700

    delete old LME scripts (still need to implment in new infrastructure)

--- scripts/LME.sh
+++
@@ -1,6 +0,0 @@
-#Usage:
-#./LME.sh DATASET_NAME.psl DATASET_NAME.data
-AVAILABLE_MEM_KB=$(cat /proc/meminfo | grep 'MemTotal' | sed 's/^[^0-9]\+\([0-9]\+\)[^0-9]\+$/\1/')
-JAVA_MEM_GB=$((${AVAILABLE_MEM_KB} / 1024 / 1024 / 5 * 5 - 5))
-
-java -Xmx${JAVA_MEM_GB}G -Xms${JAVA_MEM_GB}G -jar psl-cli-max-margin.jar -learn org.linqs.psl.application.learning.weight.maxmargin.FrankWolfe -model ${5}/${1} -data ${5}/${2} -l -D random.seed=${4} -D admmreasoner.initialconsensusvalue=ZERO -D frankwolfe.maxiter=100 -D weightlearning.randomweights=${3} -D log4j.threshold=TRACE | tee ${5}/lme_learn_${2}.log
